b856b09bb73ed178389312446922d380911e8a63
gustavo <gustavo.morais@codejr.com.br>
2019-01-31 22:48:16 -0200

    resolvidos os problemas da navbar e do quem somos

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
             'host'      => 'localhost',
             'port'      => 3306,
             'database'  => 'gpa_manga',
-            'username'  => 'root',
+            'username'  => 'october',
             'password'  => '',
             'charset'   => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
